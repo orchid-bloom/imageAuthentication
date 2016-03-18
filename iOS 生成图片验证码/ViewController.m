f118514c85242a5343dbf641f30a5601407f0a9e
tianXin <tianXin@tximac.local>
2016-03-18 10:29:58 +0800

    Initial Commit

---
+++ iOS 生成图片验证码/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOS 生成图片验证码
+//
+//  Created by tianXin on 16/3/18.
+//  Copyright © 2016年 tianXin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
